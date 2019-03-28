ec36410013f2fdae4c394dff443c0360965656c5
u1853355 <u1853355@unimail.hud.ac.uk>
2019-03-28 18:15:18 +0000

    bleh

--- src/entity/Main.java
+++ src/entity/Main.java
@@ -6,7 +6,28 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class Main extends Application {
+
+    private static ArrayList<String> likeIngredients = new ArrayList<>();
+    private static ArrayList<String> dislikeIngredients = new ArrayList<>();
+
+    public static ArrayList<String> getLikeIngredients() {
+        return likeIngredients;
+    }
+
+    public static void setLikeIngredients(ArrayList<String> likeIngredients) {
+        Main.likeIngredients = likeIngredients;
+    }
+
+    public static ArrayList<String> getDislikeIngredients() {
+        return dislikeIngredients;
+    }
+
+    public static void setDislikeIngredients(ArrayList<String> dislikeIngredients) {
+        Main.dislikeIngredients = dislikeIngredients;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
